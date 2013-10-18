12c993a4b52e6c64e49dbe45df07f1518aa2d2ec
DrStephenRacunas <Stephen.A.Racunas@gmail.com>
2013-10-18 10:26:19 -0700

    Initial Commit

---
+++ sketch.c
@@ -0,0 +1,17 @@
+
+/* Check internal capacity monitors and identify the limiting bottleneck */
+
+/* Poll request collection to see what the data priorities are */
+
+/* Formulate requests to the appropruate client sub-populations */
+
+
+
+/* Collect available data */ 
+
+
+
+/* OOB transmissions */
+/* Abort data transfer */
+/* Re-send last transmission */
+ 
